95ced8704662d4e0864c497c17ddd26073b2529d
Acer <38088640+chuynh18@users.noreply.github.com>
2018-04-30 16:23:01 -0700

    comments in line with code = baaad idea

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 "use strict"; // only the strictest code
 
 // this will be the user's input
-var inputText; // only need to declare it, not assign it any value.  It'll be assigned a value later by a prompt
+// only need to declare it, not assign it any value.  It'll be assigned a value later by a prompt
+var inputText;
 // this is what will be pushed to the screen
-var displayText = ""; // defined as an empty string because we add to displayText later.  If we only declared displayText, we'd get an error
+// defined as an empty string because we add to displayText later.  If we only declared displayText, we'd get an error
+var displayText = "";
 
 // this generates a random string beginning with "#", followed by numChars number of hex digits (that is, 0 through f)
 // we'll use it in randomizeColor()
 var genRandomColor = function(numChars) {
-    var colorArray = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]; // all the hexadecimal digits
-    var randomColor = "#"; // hex colors start with #
-    for (var i = 0; i < numChars; i++) { // loop numChars times, which gives us numChars number of hexadecimal digits
-        randomColor += colorArray[Math.floor(Math.random()*colorArray.length)]; // append random hexadecimal digits
+    // all the hexadecimal digits
+    var colorArray = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+    // hex colors start with #, so let's start off with randomColor = "#"
+    var randomColor = "#";
+    // loop numChars times, which gives us numChars number of hexadecimal digits
+    for (var i = 0; i < numChars; i++) {
+        // append random hexadecimal digits
+        randomColor += colorArray[Math.floor(Math.random()*colorArray.length)];
     }
-    return randomColor; // randomColor, you know that thing you built for us?  SPIT IT OUT.  NOW.
+    // randomColor, you know that thing you built for us?  SPIT IT OUT.  NOW.
+    return randomColor;
 };
 
 // this is the function that never ends
 var randomizeColor = function() {
     setTimeout(function() {
-        document.getElementById("textDisplay").style.color = genRandomColor(6); // we always want six to get a valid hex color
-        randomizeColor(); // always be changin' colors...
-    }, 100); // ...every 100 millis!
+        // we always want six to get a valid hex color
+        document.getElementById("textDisplay").style.color = genRandomColor(6);
+        // always be changin' colors...
+        randomizeColor();
+    // ...every 100 millis!
+    }, 100);
 };
 
 // this is how we chunk up inputText and display it seemingly one character at a time
@@ -29,25 +39,39 @@ var randomizeColor = function() {
 // okay, not dumb, but asynchronous
 var i = 0;
 var printerFunction = function() {
-    if (i < inputText.length) { // stop when you reach the end of the string given by the prompt
-        displayText = displayText.concat(inputText[i]); // this builds the strings for us
-        i++; // this makes it so the string that the above line builds for us gets longer by 1 char each time
-        document.getElementById("textDisplay").textContent = displayText; // this is what actually displays displayText on the page
-        setTimeout(function() { // we want to wait before kicking off the next cycle, so we put a delay on printerFunction() calling itself
-            printerFunction(); // keep on going until you finish "saying" what you "want" to say!
-        }, 100); // how long to wait in millis between characters
+    // this makes it so this branch only runs when there's more text to be displayed
+    if (i < inputText.length) {
+        // this builds the strings for us by concatenating the next letter in the string we want Mel to say onto displayText
+        displayText = displayText.concat(inputText[i]);
+        // this makes it so the string that the above line builds for us gets longer by 1 char each time
+        i++;
+        // this is what actually displays displayText on the page
+        document.getElementById("textDisplay").textContent = displayText;
+        // we want to wait before kicking off the next cycle, so we put a delay on printerFunction() calling itself
+        setTimeout(function() {
+            // keep on going until you finish "saying" what you "want" to say!
+            printerFunction();
+            // how long to wait in millis before kicking off printerFunction() again - this makes it so each additional letter appears 100 millis later
+        }, 100);
     }
-    else { // else we start all over again
-        i = 0; // gotta reset our variable to 0
-        displayText = ""; // it's okay to clear out displayText ASAP because it won't be visually cleared until printerFunction() calls itself
-        setTimeout(function() { // we want to wait a good long while before totally restarting
-            printerFunction(); // never shut up, even when you've already finished "speaking"!
-        }, 5000); // how long to wait in millis before starting all over again
+    
+    else {
+        // i = 0 all over again, because i is our iterator
+        i = 0;
+        // it's okay to clear out displayText ASAP because it won't be visually cleared until printerFunction() calls itself
+        displayText = "";
+        // we want to wait a good long while before totally restarting
+        setTimeout(function() {
+            // never shut up, even when you've already finished "speaking"!
+            printerFunction();
+        // how long to wait in millis before starting all over again
+        }, 5000);
     };
 };
 
 //------------------------------------------------------------------------------
 
-inputText = prompt("What does Mel say?", "Don't forget to sign in to BootCamp Spot!"); // what words do you want to put in Mel's mouth?
-randomizeColor(); // always blinking, never die
-printerFunction(); // once it begins, it'll never end
+// what words do you want to put in Mel's mouth?
+inputText = prompt("What does Mel say?", "Don't forget to sign in to BootCamp Spot!");
+randomizeColor();
+printerFunction();
