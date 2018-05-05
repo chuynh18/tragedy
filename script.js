9befc07753a207f54c2f8a0072bf454b3535bb93
5820K <38088640+chuynh18@users.noreply.github.com>
2018-05-04 23:32:18 -0700

    removed starting ... because the pause is just unnecessary

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var inputText = [
     "Did you ever hear the tragedy of Darth Plagueis the Wise?",
     "I thought not. It's not a story the Jedi would tell you.  It's a Sith legend.",
     "Darth Plagueis was a Dark Lord of the Sith, so powerful, and so wise...",
-    "...he could use the Force to influence the midichlorians to create... life.",
+    "he could use the Force to influence the midichlorians to create... life.",
     "He had such a knowledge of the dark side that he could even keep the ones he cared about from dying.",
     "The dark side of the Force is a pathway to many abilities some consider to be... unnatural.",
     "He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did.",
