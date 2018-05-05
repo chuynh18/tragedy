e068032fd3f8129f13adb08f86bf7b15d96709fd
5820K <38088640+chuynh18@users.noreply.github.com>
2018-05-05 15:26:05 -0700

    make it so user can't trigger the event listener over and over

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 "use strict";
 
 var input = [
-    {text: "Did you ever hear the tragedy of Darth Plagueis the Wise?", audio: "1.webm", delay: 83},
-    {text: "I thought not. It's not a story the Jedi would tell you.  It's a Sith legend.", audio: "2.webm", delay: 70},
-    {text: "Darth Plagueis was a Dark Lord of the Sith, so powerful, and so wise", audio: "3.webm", delay: 78},
-    {text: "He could use the Force to influence the midichlorians.. to create.. life.", audio: "4.webm", delay: 65},
-    {text: "He had such a knowledge of the dark side, he could even keep the ones he cared about... from dying.", audio: "5.webm", delay: 80},
-    {text: "The dark side of the Force is a pathway to many abilities some consider to be unnatural.", audio: "6.webm", delay: 77},
-    {text: "He became so powerful... the only thing he was afraid of was.. losing his power which eventually of course he did.", audio: "7.webm", delay: 62},
-    {text: "Unfortunately, he taught his apprentice everything he knew... then his apprentice killed him in his sleep.", audio: "8.webm", delay: 65},
-    {text: "Ironic.. he could save others from death.. but not himself.", audio: "9.webm", delay: 66}
+    {text: "Did you ever hear the tragedy of Darth Plagueis the Wise?", audio: "1.webm", delay: 83, lineDelay: 2000},
+    {text: "I thought not. It's not a story the Jedi would tell you.  It's a Sith legend.", audio: "2.webm", delay: 70, lineDelay: 1000},
+    {text: "Darth Plagueis was a Dark Lord of the Sith, so powerful, and so wise", audio: "3.webm", delay: 81, lineDelay: 700},
+    {text: "He could use the Force to influence the midichlorians.. to create.. life.", audio: "4.webm", delay: 65, lineDelay: 2000},
+    {text: "He had such a knowledge of the dark side, he could even keep the ones he cared about... from dying.", audio: "5.webm", delay: 80, lineDelay: 2000},
+    {text: "The dark side of the Force is a pathway to many abilities some consider to be unnatural.", audio: "6.webm", delay: 79, lineDelay: 2000},
+    {text: "He became so powerful... the only thing he was afraid of was.. losing his power which eventually of course he did.", audio: "7.webm", delay: 62, lineDelay: 2000},
+    {text: "Unfortunately, he taught his apprentice everything he knew... then his apprentice killed him in his sleep.", audio: "8.webm", delay: 65, lineDelay: 2000},
+    {text: "Ironic.. he could save others from death.. but not himself.", audio: "9.webm", delay: 66, lineDelay: 2000}
 ];
 var displayText = "";
 
@@ -67,7 +67,7 @@ var printerFunction = function() {
             setTimeout(function() {
                 document.getElementById("palpatine").src="palpysmalltalk.gif";
                 printerFunction();
-            }, 2000);
+            }, input[currentLine].lineDelay);
         }
         // and NEVER stop talking!
         else {
@@ -84,7 +84,8 @@ var printerFunction = function() {
 
 // ---------------------------------- function calls ----------------------------------
 
-document.addEventListener('click', function() {
+document.getElementById("start").addEventListener('click', function() {
+    document.getElementById("start").textContent = "";
     document.getElementById("palpatine").src="palpysmalltalk.gif";
     printerFunction();
 });
