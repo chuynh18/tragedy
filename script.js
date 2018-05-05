50c5a5303bc184e3d6c28ef978d0e0c1ca3792e7
5820K <38088640+chuynh18@users.noreply.github.com>
2018-05-04 22:02:33 -0700

    first working version of tragedy of darth plagueis the wise

--- script.js
+++ script.js
@@ -1,62 +1,27 @@
 "use strict";
 
-var inputText;
-var displayText = "";
-
-// add more characters to find and replace here as they are discovered
-const charTable = [
-    {find: "%20", replace: " "},
-    {find: "%21", replace: "!"},
-    {find: "%22", replace: '"'},
-    {find: "%27", replace: "'"},
-    {find: "%2C", replace: ","},
-    {find: "%3A", replace: ":"},
-    {find: "%3C", replace: "<"},
-    {find: "%3E", replace: ">"},
-    {find: "%3F", replace: "?"},
-    {find: "%40", replace: "@"},
-    {find: "%60", replace: "`"},
-    {find: "+", replace: " "} // did not happen locally, but space was replaced by +
+var inputText = [
+    "Did you ever hear the tragedy of Darth Plagueis the Wise?",
+    "I thought not. It's not a story the Jedi would tell you.  It's a Sith legend.",
+    "Darth Plagueis was a Dark Lord of the Sith, so powerful, and so wise...",
+    "...he could use the Force to influence the midichlorians to create... life.",
+    "He had such a knowledge of the dark side that he could even keep the ones he cared about from dying.",
+    "The dark side of the Force is a pathway to many abilities some consider to be... unnatural.",
+    "He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did.",
+    "Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep.",
+    "It's ironic... he could save others from death... but not himself."
 ];
 
-var parseURL = function() {
-    var URL = window.location.href;
-    if (URL.indexOf("q=") !== -1) {
-        var spliced = URL.substr(URL.search("q=") + 2);
-        for (var i = 0; i < charTable.length; i++) {
-            while (spliced.indexOf(charTable[i].find) !== -1) {
-                spliced = spliced.replace(charTable[i].find, charTable[i].replace)
-            };
-        };
-        inputText = spliced;
-    }
-    else {
-        inputText = prompt("What does Mel say?", "Don't forget to sign in to BootCamp Spot!");
-    };
-};
+var currentLine = 0;
 
-var genRandomColor = function(numChars) {
-    var colorArray = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
-    var randomColor = "#";
-    for (var i = 0; i < numChars; i++) {
-        randomColor += colorArray[Math.floor(Math.random()*colorArray.length)];
-    }
-    return randomColor;
-};
-
-var randomizeColor = function() {
-    setTimeout(function() {
-        document.getElementById("textDisplay").style.color = genRandomColor(6);
-        randomizeColor();
-    }, 100);
-};
+var displayText = "";
 
 // this is how we chunk up inputText and display it one character at a time
 // we don't (can't!) use a for loop, because JavaScript is dumb.  I meant asynchronous.
 var i = 0;
 var printerFunction = function() {
-    if (i < inputText.length) {
-        displayText = displayText.concat(inputText[i]);
+    if (i < inputText[currentLine].length) {
+        displayText = displayText.concat(inputText[currentLine][i]);
         i++;
         document.getElementById("textDisplay").textContent = displayText;
         setTimeout(function() {
@@ -64,16 +29,28 @@ var printerFunction = function() {
         }, 50);
     }
     else {
-        i = 0;
-        displayText = "";
-        setTimeout(function() {
-            printerFunction();
-        }, 5000);
+        if (currentLine < inputText.length) {
+            i = 0;
+            currentLine++;
+            displayText = "";
+            document.getElementById("palpatine").src="palpysmallstill.jpg";
+            setTimeout(function() {
+                printerFunction();
+                document.getElementById("palpatine").src="palpysmalltalk.gif";
+            }, 5000);
+        }
+        else {
+            currentLine = 0;
+            document.getElementById("palpatine").src="palpysmallstill.jpg";
+            setTimeout(function() {
+                printerFunction();
+                document.getElementById("palpatine").src="palpysmalltalk.gif";
+            }, 10000);
+        };
     };
+
 };
 
 // ---------------------------------- function calls ----------------------------------
 
-parseURL(); // runs once, gets text to write either through URL or prompt
-randomizeColor(); // infinite loop
 printerFunction(); // infinite loop
