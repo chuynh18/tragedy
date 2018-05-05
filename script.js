aa3132002a773239ab8a89d1bac0dc3934cd0395
5820K <38088640+chuynh18@users.noreply.github.com>
2018-05-05 14:28:00 -0700

    reduce pause between lines and pause before looping by 1 sec each

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var inputText = [
     "Did you ever hear the tragedy of Darth Plagueis the Wise?",
     "I thought not. It's not a story the Jedi would tell you.  It's a Sith legend.",
     "Darth Plagueis was a Dark Lord of the Sith, so powerful, and so wise...",
-    "he could use the Force to influence the midichlorians to create... life.",
+    "He could use the Force to influence the midichlorians to create... life.",
     "He had such a knowledge of the dark side that he could even keep the ones he cared about from dying.",
     "The dark side of the Force is a pathway to many abilities some consider to be... unnatural.",
     "He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did.",
@@ -57,7 +57,7 @@ var printerFunction = function() {
             setTimeout(function() {
                 document.getElementById("palpatine").src="palpysmalltalk.gif";
                 printerFunction();
-            }, 4000);
+            }, 3000);
         }
         // and NEVER stop talking!
         else {
@@ -66,7 +66,7 @@ var printerFunction = function() {
             setTimeout(function() {
                 document.getElementById("palpatine").src="palpysmalltalk.gif";
                 printerFunction();
-            }, 8000);
+            }, 7000);
         };
     };
 
