9855dd9502a23a7b9325d551081545401f86c085
5820K <38088640+chuynh18@users.noreply.github.com>
2018-05-05 15:34:26 -0700

    timing changes to make text and audio sync even better

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 "use strict";
 
 var input = [
-    {text: "Did you ever hear the tragedy of Darth Plagueis the Wise?", audio: "1.webm", delay: 83, lineDelay: 2000},
-    {text: "I thought not. It's not a story the Jedi would tell you.  It's a Sith legend.", audio: "2.webm", delay: 70, lineDelay: 1000},
-    {text: "Darth Plagueis was a Dark Lord of the Sith, so powerful, and so wise", audio: "3.webm", delay: 81, lineDelay: 700},
-    {text: "He could use the Force to influence the midichlorians.. to create.. life.", audio: "4.webm", delay: 65, lineDelay: 2000},
+    {text: "Did you ever hear the tragedy of Darth Plagueis the Wise?", audio: "1.webm", delay: 80, lineDelay: 2000},
+    {text: "I thought not. It's not a story the Jedi would tell you.  It's a Sith legend.", audio: "2.webm", delay: 70, lineDelay: 600},
+    {text: "Darth Plagueis was a Dark Lord of the Sith, so powerful, and so wise", audio: "3.webm", delay: 85, lineDelay: 1100},
+    {text: "He could use the Force to influence the midichlorians.. to create.. life.", audio: "4.webm", delay: 65, lineDelay: 1000},
     {text: "He had such a knowledge of the dark side, he could even keep the ones he cared about... from dying.", audio: "5.webm", delay: 80, lineDelay: 2000},
-    {text: "The dark side of the Force is a pathway to many abilities some consider to be unnatural.", audio: "6.webm", delay: 79, lineDelay: 2000},
-    {text: "He became so powerful... the only thing he was afraid of was.. losing his power which eventually of course he did.", audio: "7.webm", delay: 62, lineDelay: 2000},
-    {text: "Unfortunately, he taught his apprentice everything he knew... then his apprentice killed him in his sleep.", audio: "8.webm", delay: 65, lineDelay: 2000},
-    {text: "Ironic.. he could save others from death.. but not himself.", audio: "9.webm", delay: 66, lineDelay: 2000}
+    {text: "The dark side of the Force is a pathway to many abilities some consider to be unnatural.", audio: "6.webm", delay: 85, lineDelay: 1000},
+    {text: "He became so powerful... the only thing he was afraid of was.. losing his power which eventually of course he did.", audio: "7.webm", delay: 62, lineDelay: 1000},
+    {text: "Unfortunately, he taught his apprentice everything he knew... then his apprentice killed him in his sleep.", audio: "8.webm", delay: 64, lineDelay: 1000},
+    {text: "Ironic.. he could save others from death.. but not himself.", audio: "9.webm", delay: 66, lineDelay: 500}
 ];
 var displayText = "";
 
@@ -67,7 +67,7 @@ var printerFunction = function() {
             setTimeout(function() {
                 document.getElementById("palpatine").src="palpysmalltalk.gif";
                 printerFunction();
-            }, input[currentLine].lineDelay);
+            }, input[currentLine-1].lineDelay);
         }
         // and NEVER stop talking!
         else {
